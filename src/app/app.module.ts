e69a09b5f2c27048fd6f5b4112718286df3ec2b1
Andrii Polchaninov <hundd@rambler.ru>
2018-11-09 20:25:03 +0200

    Task 02 - add Material and Flex-Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './products/components/product/product.component';
 import { CartComponent } from './cart/components/cart/cart.component';
 import { ProductListComponent } from './products/components/product-list/product-list.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, CartComponent, ProductListComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    CartComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FlexLayoutModule,
+    MaterialModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
